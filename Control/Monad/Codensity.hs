9558165c7280fca8d04c1d167c4ef6aa93aaacbc
Edward Kmett <ekmett@gmail.com>
2011-06-08 21:02:58 -0400

    renamed (Co)densityT -> (Co)density and added instance MonadReader e m => MonadState e (Codensity m)

--- Control/Monad/Codensity.hs
+++ Control/Monad/Codensity.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE Rank2Types, FlexibleInstances, MultiParamTypeClasses, UndecidableInstances #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Control.Monad.Codensity
@@ -11,60 +11,60 @@
 --
 ----------------------------------------------------------------------------
 module Control.Monad.Codensity
-  ( CodensityT(..)
-  , lowerCodensityT
-  , codensityTToAdjunction
-  , adjunctionToCodensityT
+  ( Codensity(..)
+  , lowerCodensity
+  , codensityToAdjunction
+  , adjunctionToCodensity
   ) where
 
 import Control.Applicative
+import Control.Monad.Reader.Class
+import Control.Monad.State.Class
 import Control.Monad (ap, MonadPlus(..))
 import Data.Functor.Adjunction
 import Data.Functor.Apply
 import Control.Monad.Trans.Class
 import Control.Monad.IO.Class
 
-{-
-type Codensity = CodensityT Identity
-codensity :: (forall b. (a -> b) -> b) -> Codensity a
-runCodensity :: Codensity a -> (a -> b) -> a
--}
+newtype Codensity m a = Codensity { runCodensity :: forall b. (a -> m b) -> m b }
 
-newtype CodensityT m a = CodensityT { runCodensityT :: forall b. (a -> m b) -> m b }
+instance Functor (Codensity k) where
+  fmap f (Codensity m) = Codensity (\k -> m (k . f))
 
-instance Functor (CodensityT k) where
-  fmap f (CodensityT m) = CodensityT (\k -> m (k . f))
-
-instance Apply (CodensityT f) where
+instance Apply (Codensity f) where
   (<.>) = ap
 
-instance Applicative (CodensityT f) where
-  pure x = CodensityT (\k -> k x)
+instance Applicative (Codensity f) where
+  pure x = Codensity (\k -> k x)
   (<*>) = ap
 
-instance Monad (CodensityT f) where
-  return x = CodensityT (\k -> k x)
-  m >>= k = CodensityT (\c -> runCodensityT m (\a -> runCodensityT (k a) c))
+instance Monad (Codensity f) where
+  return x = Codensity (\k -> k x)
+  m >>= k = Codensity (\c -> runCodensity m (\a -> runCodensity (k a) c))
 
-instance MonadIO m => MonadIO (CodensityT m) where
+instance MonadIO m => MonadIO (Codensity m) where
   liftIO = lift . liftIO 
 
-instance MonadTrans CodensityT where
-  lift m = CodensityT (m >>=)
+instance MonadTrans Codensity where
+  lift m = Codensity (m >>=)
 
-instance Alternative v => Alternative (CodensityT v) where
-  empty                         = CodensityT (\_ -> empty)
-  CodensityT m <|> CodensityT n = CodensityT (\k -> m k <|> n k)
+instance Alternative v => Alternative (Codensity v) where
+  empty                         = Codensity (\_ -> empty)
+  Codensity m <|> Codensity n = Codensity (\k -> m k <|> n k)
 
-instance MonadPlus v => MonadPlus (CodensityT v) where
-  mzero                             = CodensityT (\_ -> mzero)
-  CodensityT m `mplus` CodensityT n = CodensityT (\k -> m k `mplus` n k)
+instance MonadPlus v => MonadPlus (Codensity v) where
+  mzero                             = Codensity (\_ -> mzero)
+  Codensity m `mplus` Codensity n = Codensity (\k -> m k `mplus` n k)
  
-lowerCodensityT :: Monad m => CodensityT m a -> m a
-lowerCodensityT a = runCodensityT a return
+lowerCodensity :: Monad m => Codensity m a -> m a
+lowerCodensity a = runCodensity a return
 
-codensityTToAdjunction :: Adjunction f g => CodensityT g a -> g (f a)
-codensityTToAdjunction r = runCodensityT r unit
+codensityToAdjunction :: Adjunction f g => Codensity g a -> g (f a)
+codensityToAdjunction r = runCodensity r unit
 
-adjunctionToCodensityT :: Adjunction f g => g (f a) -> CodensityT g a
-adjunctionToCodensityT f = CodensityT (\a -> fmap (rightAdjunct a) f)
+adjunctionToCodensity :: Adjunction f g => g (f a) -> Codensity g a
+adjunctionToCodensity f = Codensity (\a -> fmap (rightAdjunct a) f)
+
+instance MonadReader r m => MonadState r (Codensity m) where
+  get = Codensity (ask >>=)
+  put s = Codensity (\k -> local (const s) (k ()))
